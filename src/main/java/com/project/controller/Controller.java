648fc5951f0a0aa300beb201fa038963a664962b
laki5000 <lakatos816@gmail.com>
2024-04-17 14:52:20 +0200

    test commit

---
+++ src/main/java/com/project/controller/Controller.java
@@ -0,0 +1,14 @@
+package com.project.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("${api.base.path}")
+public class Controller {
+    @GetMapping("/endpoint")
+    public String getEndpoint() {
+        return "GET request handled at: ${api.basePath}/endpoint";
+    }
+}
