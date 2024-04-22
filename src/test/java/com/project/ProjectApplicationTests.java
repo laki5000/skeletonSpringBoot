e716eb1399898b1ec00a269eef4dc5088bd64b92
laki5000 <lakatos816@gmail.com>
2024-04-22 12:51:17 +0200

    testing test job

--- src/test/java/com/project/ProjectApplicationTests.java
+++ src/test/java/com/project/ProjectApplicationTests.java
@@ -3,10 +3,13 @@ package com.project;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.fail;
+
 @SpringBootTest
 class ProjectApplicationTests {
 
     @Test
     void contextLoads() {
+        fail("Teszt sikertelen állítás miatt.");
     }
 }
