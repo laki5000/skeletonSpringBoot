ac19bfba4eba244c8865cd76052704f0190e7f2c
laki5000 <lakatos816@gmail.com>
2024-04-23 09:48:39 +0200

    multilingualism implementation

--- src/main/java/com/example/utils/controller/BaseControllerForExceptionHandling.java
+++ src/main/java/com/example/utils/controller/BaseControllerForExceptionHandling.java
@@ -1,6 +1,8 @@
 package com.example.utils.controller;
 
 import com.example.utils.dto.response.ErrorResponse;
+import com.example.utils.service.MessageService;
+import lombok.Getter;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,7 +10,19 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 /** Base controller for exception handling. */
 @Log4j2
+@Getter
 public abstract class BaseControllerForExceptionHandling {
+    private final MessageService messageService;
+
+    /**
+     * Constructor.
+     *
+     * @param messageService the message service
+     */
+    public BaseControllerForExceptionHandling(MessageService messageService) {
+        this.messageService = messageService;
+    }
+
     /**
      * Handles generic exceptions.
      *
@@ -20,7 +34,7 @@ public abstract class BaseControllerForExceptionHandling {
         log.error("Handling generic exception", ex);
 
         HttpStatus statusCode = HttpStatus.INTERNAL_SERVER_ERROR;
-        ErrorResponse errorResponse = new ErrorResponse(statusCode.value(), "Internal Server Error: " + ex.getMessage());
+        ErrorResponse errorResponse = new ErrorResponse(statusCode.value(), messageService.getMessage("base.controller.exception.response01") + ex.getMessage());
 
         log.error("Returning error response: {}", errorResponse);
 
