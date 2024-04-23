56f3f5e0b3d79d5cfec580564bf27191f3a8382f
laki5000 <lakatos816@gmail.com>
2024-04-23 16:09:30 +0200

    switched username to unique and not null, password to not null

--- src/main/java/com/example/user/model/User.java
+++ src/main/java/com/example/user/model/User.java
@@ -1,6 +1,7 @@
 package com.example.user.model;
 
 import com.example.utils.model.BaseModelForCreation;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import lombok.Getter;
 
@@ -8,6 +9,9 @@ import lombok.Getter;
 @Entity
 @Getter
 public class User extends BaseModelForCreation {
+    @Column(nullable = false, unique = true)
     private String username;
+
+    @Column(nullable = false)
     private String password;
 }
