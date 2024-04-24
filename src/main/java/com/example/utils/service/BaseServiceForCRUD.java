d5ca2894ffeb0ad5e26cd280a0c10b871b8e061c
Laki <lakatos.bence.magicpixel@gmail.com>
2024-04-24 11:02:55 +0200

    finished the implementation of abstract create method

--- src/main/java/com/example/utils/service/BaseServiceForCRUD.java
+++ src/main/java/com/example/utils/service/BaseServiceForCRUD.java
@@ -2,14 +2,12 @@ package com.example.utils.service;
 
 import com.example.exception.MyConflictException;
 import com.example.exception.MyNotFoundException;
+import com.example.exception.MyNotModifiedException;
 import com.example.utils.mapper.BaseMapper;
 import lombok.Getter;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.Optional;
-import java.util.Set;
 
 /** Base service class for CRUD operations. */
 @Log4j2
@@ -17,6 +15,7 @@ import java.util.Set;
 public abstract class BaseServiceForCRUD<T, CRQ, URQ, GRP> {
     private final MessageService messageService;
     private final BaseMapper<T, CRQ, URQ, GRP> mapper;
+    private final Class<T> clazz;
     private final String tClassName;
 
     protected abstract JpaRepository<T, Long> getRepository();
@@ -29,6 +28,7 @@ public abstract class BaseServiceForCRUD<T, CRQ, URQ, GRP> {
     public BaseServiceForCRUD(MessageService messageService, BaseMapper<T, CRQ, URQ, GRP> mapper, Class<T> entityType) {
         this.messageService = messageService;
         this.mapper = mapper;
+        this.clazz = entityType;
         this.tClassName = entityType.getSimpleName();
     }
 
@@ -70,16 +70,21 @@ public abstract class BaseServiceForCRUD<T, CRQ, URQ, GRP> {
 
         Object validationResult = validateUpdate(update);
 
-        System.out.println(Object.class);
-        System.out.println(tClassName);
-
         if (validationResult instanceof String) {
             throw new MyNotFoundException(messageService.getMessage(validationResult.toString()));
         }
 
+        Object updateMessage = doUpdate(clazz.cast(validationResult), update);
+
+        if (updateMessage instanceof String) {
+            throw new MyNotModifiedException(messageService.getMessage(updateMessage.toString()));
+        }
+
+        GRP createdResponse = mapper.toGetResponse(clazz.cast(validationResult));
+
         log.info("{} updated with id: {}", tClassName, id);
 
-        return null;
+        return createdResponse;
     }
 
     /**
@@ -105,4 +110,13 @@ public abstract class BaseServiceForCRUD<T, CRQ, URQ, GRP> {
      * @return the id
      */
     protected abstract Long getIdFromUpdateRequest(URQ entity);
+
+    /**
+     * Update the entity.
+     *
+     * @param entity the entity to update
+     * @param update the update request
+     * @return If the return value is a String, it indicates an error occurred during validation. If it's a null, it means everything is okay.
+     */
+    protected abstract Object doUpdate(T entity, URQ update);
 }
