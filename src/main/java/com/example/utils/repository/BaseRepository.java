2d7d3a127f316aa688bc7ecf6bd29da739e221dc
Laki <lakatos.bence.magicpixel@gmail.com>
2024-04-24 13:53:21 +0200

    refactor abstract getting, now you can add multiple request params

--- src/main/java/com/example/utils/repository/BaseRepository.java
+++ src/main/java/com/example/utils/repository/BaseRepository.java
@@ -12,6 +12,7 @@ import org.springframework.data.repository.NoRepositoryBean;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 
 /**
@@ -25,17 +26,23 @@ public interface BaseRepository<T, ID extends Serializable> extends JpaRepositor
     /**
      * Find all entities with criteria.
      *
-     * @param page           the page number
-     * @param limit          the page size
-     * @param id             the ID
-     * @param orderBy        the order by field
-     * @param orderDirection the order direction
+     * @param params         the search parameters
      * @return page of entities
      */
     default Page<T> findAllWithCriteria(
-                                        int page, int limit, Long id, String orderBy, String orderDirection) {
+            Map<String, String> params) {
+        int page = params.get("page") != null && !params.get("page").isEmpty() ? Integer.parseInt(params.get("page")) : 0;
+        int limit = params.get("limit") != null && !params.get("page").isEmpty() ? Integer.parseInt(params.get("limit")) : 10;
+        String orderBy = params.get("orderBy") != null && !params.get("orderBy").isEmpty() ? params.get("orderBy") : "id";
+        String orderDirection = params.get("orderDirection") != null && !params.get("orderDirection").isEmpty() ? params.get("orderDirection") : "asc";
+
+        params.remove("page");
+        params.remove("limit");
+        params.remove("orderBy");
+        params.remove("orderDirection");
+
         Pageable pageable = PageRequest.of(page, limit);
-        Specification<T> spec = buildSpecification(id, orderBy, orderDirection);
+        Specification<T> spec = buildSpecification(params, orderBy, orderDirection);
 
         return findAll(spec, pageable);
     }
@@ -43,17 +50,28 @@ public interface BaseRepository<T, ID extends Serializable> extends JpaRepositor
     /**
      * Build a specification.
      *
-     * @param id             the ID
-     * @param orderBy        the order by field
+     * @param params         the search parameters
+     * @param orderBy        the field to order by
      * @param orderDirection the order direction
      * @return the specification
      */
-    default Specification<T> buildSpecification(Long id, String orderBy, String orderDirection) {
+    default Specification<T> buildSpecification(Map<String, String> params, String orderBy, String orderDirection) {
         return (root, query, criteriaBuilder) -> {
             List<Predicate> predicates = new ArrayList<>();
 
-            if (Objects.nonNull(id)) {
-                predicates.add(criteriaBuilder.equal(root.get("id"), id));
+            for (Map.Entry<String, String> entry : params.entrySet()) {
+                String key = entry.getKey();
+                String value = entry.getValue();
+
+                if (value == null || value.isEmpty()) {
+                    continue;
+                }
+
+                if (root.get(key).getJavaType() == String.class) {
+                    predicates.add(criteriaBuilder.like(root.get(key), value + "%"));
+                } else {
+                    predicates.add(criteriaBuilder.equal(root.get(key), value));
+                }
             }
 
             if (Objects.nonNull(orderBy) && Objects.nonNull(orderDirection)) {
