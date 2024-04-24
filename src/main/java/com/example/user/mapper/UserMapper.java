9cee50d57f90a913705789ffd6cd944bad29a87a
Laki <lakatos.bence.magicpixel@gmail.com>
2024-04-24 09:01:01 +0200

    bugfix and spotless

--- src/main/java/com/example/user/mapper/UserMapper.java
+++ src/main/java/com/example/user/mapper/UserMapper.java
@@ -6,8 +6,15 @@ import com.example.utils.mapper.BaseMapper;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
+/** Mapper for user entities. */
 @Mapper
 public interface UserMapper extends BaseMapper<User, UserCreateRequest> {
+    /**
+     * Maps a user create request to a user entity.
+     *
+     * @param dto the user create request
+     * @return the user entity
+     */
     @Mapping(target = "username", source = "dto.username")
     @Mapping(target = "password", source = "dto.password")
     @Mapping(target = "createdBy", source = "dto.username")
--- src/main/java/com/example/user/repository/UserRepository.java
+++ src/main/java/com/example/user/repository/UserRepository.java
@@ -4,7 +4,7 @@ import com.example.user.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-/** A repository class for managing users. */
+/** A repository interface for managing users. */
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByUsername(String username);
