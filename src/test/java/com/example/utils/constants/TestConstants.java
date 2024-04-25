0fa728a57a8e09ae64b81fcec9e9b77bf0f7df67
laki5000 <lakatos816@gmail.com>
2024-04-25 15:47:57 +0200

    made tests for MessageService

--- src/test/java/com/example/utils/constants/TestConstants.java
+++ src/test/java/com/example/utils/constants/TestConstants.java
@@ -1,4 +1,10 @@
 package com.example.utils.constants;
 
+import java.util.Locale;
+
 public class TestConstants {
+    public static final String validKey = "test.valid.key";
+    public static final String invalidKey = "test.invalid.key";
+    public static final String expectedMessage = "Test message";
+    public static final Locale locale = Locale.ENGLISH;
 }
