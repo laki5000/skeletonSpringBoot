86b3b971d6e184330b7ec0442b05859d33c1b7d4
Laki <lakatos.bence.magicpixel@gmail.com>
2024-04-26 15:28:10 +0200

    added tests, need more!

--- src/main/java/com/example/user/dto/request/UserUpdateRequest.java
+++ src/main/java/com/example/user/dto/request/UserUpdateRequest.java
@@ -1,9 +1,11 @@
 package com.example.user.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 /** Dto class for update a user. */
 @Getter
+@AllArgsConstructor
 public class UserUpdateRequest {
     private Long id;
     private String password;
--- src/test/java/com/example/user/UserTestUtils.java
+++ src/test/java/com/example/user/UserTestUtils.java
@@ -1,6 +1,7 @@
 package com.example.user;
 
 import com.example.user.dto.request.UserCreateRequest;
+import com.example.user.dto.request.UserUpdateRequest;
 import com.example.user.dto.response.UserGetResponse;
 import com.example.user.model.User;
 
@@ -9,6 +10,10 @@ import java.time.Instant;
 public class UserTestUtils {
     public static UserCreateRequest getUserCreateRequest(String username, String password) {
         return new UserCreateRequest(username, password);
+    }
+
+    public static UserUpdateRequest getUserUpdateRequest(Long id, String password) {
+        return new UserUpdateRequest(id, password);
     }
 
     public static User getUser(Long id, String username, String password, Instant createdAt, String createdBy, Instant updatedAt, String updatedBy) {
