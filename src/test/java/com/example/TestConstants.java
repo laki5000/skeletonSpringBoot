3f51bee0581f9a932d8de2d56b3a32338ed31c24
laki5000 <lakatos816@gmail.com>
2024-05-06 11:59:56 +0200

    integration tests part 1:)

--- src/test/java/com/example/TestConstants.java
+++ src/test/java/com/example/TestConstants.java
@@ -8,8 +8,9 @@ public class TestConstants {
     public static final String EXPECTED_MESSAGE = "Test message";
     public static final Locale LOCALE = Locale.ENGLISH;
     public static final Long ID = 1L;
-    public static final Long INVALID_ID = 2L;
     public static final String USERNAME = "username";
+    public static final String INVALID_USERNAME = "iu";
     public static final String PASSWORD = "Val1dP@ssword";
+    public static final String INVALID_PASSWORD = "invalid";
     public static final String MODIFIED_PASSWORD = "M0d1f13dP@ssword";
 }
